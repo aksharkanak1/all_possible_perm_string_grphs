f19fe3da5614b34da62dfb2acce0e6bef5242375
akshar.kanak <akshar.kanak1@gmail.com>
2014-05-12 21:08:41 +0530

    Added multi processor support

--- strings_perm_main.cpp
+++ strings_perm_main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
-
+#include<string.h>
 extern int gerAllPosStrings(char *str);
+
+int mp =0;
 
 int main(int argc , char *argv[])
 {
-    if(2 != argc)
+    if(3 != argc)
     {
         std::cout <<"\nInvalid number of arguments\n" ;
         return -1;
     }  
 
-    gerAllPosStrings(argv[1]);
+
+    if(0==strcmp("mp",argv[1]))
+         mp =1;
+        
+
+    gerAllPosStrings(argv[2]);
 }
