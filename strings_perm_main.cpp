bee64a836e03f506057833c528363e273ecb6080
root <root@localhost.localdomain>
2015-11-21 11:19:46 -0500

    Revert "Added multi processor support"
    This reverts commit f19fe3da5614b34da62dfb2acce0e6bef5242375.

--- strings_perm_main.cpp
+++ strings_perm_main.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include<string.h>
-extern int gerAllPosStrings(char *str);
 
-int mp =0;
+extern int gerAllPosStrings(char *str);
 
 int main(int argc , char *argv[])
 {
-    if(3 != argc)
+    if(2 != argc)
     {
         std::cout <<"\nInvalid number of arguments\n" ;
         return -1;
     }  
 
-
-    if(0==strcmp("mp",argv[1]))
-         mp =1;
-        
-
-    gerAllPosStrings(argv[2]);
+    gerAllPosStrings(argv[1]);
 }
